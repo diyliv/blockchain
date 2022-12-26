b994cef3f99dd03f4ce80032c04a8ab27c9127fc
diyliv <onlogn081@gmail.com>
2022-12-26 16:14:33 +0300

    added constructor

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -5,3 +5,11 @@ type Transaction struct {
 	Recipient string  `json:"recipient"`
 	Amount    float64 `json:"amount"`
 }
+
+func NewTransaction(sender, recipient string, amount float64) *Transaction {
+	return &Transaction{
+		Sender:    sender,
+		Recipient: recipient,
+		Amount:    amount,
+	}
+}
